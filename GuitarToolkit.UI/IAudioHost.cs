60834f023f961ff0f7f1491b85d72a1ae3b00f14
Аникьев Евгений Юрьевич <anikjevjenya@gmail.com>
2026-04-21 00:12:44 +0300

    some minor update in comments

--- GuitarToolkit.UI/IAudioHost.cs
+++ GuitarToolkit.UI/IAudioHost.cs
@@ -1,17 +1,10 @@
 namespace GuitarToolkit.UI;
 
-/// <summary>
-/// Абстракция аудио-хоста: позволяет UI воспроизводить звук
-/// не зная, работает он внутри VST-плагина или standalone-приложения.
-/// </summary>
 public interface IAudioPlayback
 {
-    /// <summary>Частота дискретизации (Гц).</summary>
     int SampleRate { get; }
 
-    /// <summary>Поставить буфер сэмплов в очередь на воспроизведение.</summary>
     void PlaySamples(float[] samples);
 
-    /// <summary>Остановить текущее воспроизведение.</summary>
     void StopPlayback();
 }
