9bf3db137b6dce2088191150eefd969267e1135c
Аникьев Евгений Юрьевич <anikjevjenya@gmail.com>
2026-04-29 17:43:09 +0300

    fix

--- GuitarToolkit.Plugin/GuitarToolkitPlugin.cs
+++ GuitarToolkit.Plugin/GuitarToolkitPlugin.cs
@@ -1,5 +1,4 @@
 using System.Windows.Controls;
-using System.Threading;
 using AudioPlugSharp;
 using AudioPlugSharpWPF;
 using GuitarToolkit.Core.Services;
@@ -18,15 +17,6 @@ public class GuitarToolkitPlugin : AudioPluginWPF, IAudioPlayback
     private float[]? _playbackBuffer;
     private int _playbackPos;
 
-    private float[] _inputFloatBuffer = Array.Empty<float>();
-    private float[] _metronomeBuffer = Array.Empty<float>();
-
-    private static void EnsureBuffer(ref float[] buffer, int length)
-    {
-        if (buffer.Length < length)
-            buffer = new float[length];
-    }
-
 
     public int SampleRate
     {
@@ -38,13 +28,13 @@ public class GuitarToolkitPlugin : AudioPluginWPF, IAudioPlayback
 
     public void PlaySamples(float[] samples)
     {
+        _playbackBuffer = samples;
         _playbackPos = 0;
-        Volatile.Write(ref _playbackBuffer, samples);
     }
 
     public void StopPlayback()
     {
-        Volatile.Write(ref _playbackBuffer, null);
+        _playbackBuffer = null;
         _playbackPos = 0;
     }
 
@@ -86,70 +76,58 @@ public class GuitarToolkitPlugin : AudioPluginWPF, IAudioPlayback
 
     public override void Process()
     {
-        Span<double> input = _monoInput.GetAudioBuffer(0);
-        Span<double> outL = _stereoOutput.GetAudioBuffer(0);
-        Span<double> outR = _stereoOutput.GetAudioBuffer(1);
-        int len = input.Length;
-
-        EnsureBuffer(ref _inputFloatBuffer, len);
-        EnsureBuffer(ref _metronomeBuffer, len);
-
-        for (int i = 0; i < len; i++)
-        {
-            double sample = input[i];
-            outL[i] = sample;
-            outR[i] = sample;
-            _inputFloatBuffer[i] = (float)sample;
-        }
-
         try
         {
-            Tuner.ProcessSamples(_inputFloatBuffer, len);
-        }
-        catch (Exception ex)
-        {
-            System.Diagnostics.Debug.WriteLine($"Tuner processing error: {ex.Message}");
-        }
-
-        try
-        {
-            Array.Clear(_metronomeBuffer, 0, len);
-            Metronome.ProcessBlock(_metronomeBuffer, len);
+            Span<double> input = _monoInput.GetAudioBuffer(0);
+            Span<double> outL = _stereoOutput.GetAudioBuffer(0);
+            Span<double> outR = _stereoOutput.GetAudioBuffer(1);
+            int len = input.Length;
 
             for (int i = 0; i < len; i++)
             {
-                double click = _metronomeBuffer[i];
-                outL[i] += click;
-                outR[i] += click;
+                outL[i] = input[i];
+                outR[i] = input[i];
             }
-        }
-        catch (Exception ex)
-        {
-            System.Diagnostics.Debug.WriteLine($"Metronome processing error: {ex.Message}");
-        }
 
-        try
-        {
-            var buf = Volatile.Read(ref _playbackBuffer);
+            try
+            {
+                float[] floatBuf = new float[len];
+                for (int i = 0; i < len; i++)
+                    floatBuf[i] = (float)input[i];
+                Tuner.ProcessSamples(floatBuf, len);
+            }
+            catch { }
+
+            try
+            {
+                float[] metroBuf = new float[len];
+                Metronome.ProcessBlock(metroBuf, len);
+                for (int i = 0; i < len; i++)
+                {
+                    outL[i] += metroBuf[i];
+                    outR[i] += metroBuf[i];
+                }
+            }
+            catch { }
+
+            var buf = _playbackBuffer;
             if (buf != null && _playbackPos < buf.Length)
             {
                 for (int i = 0; i < len && _playbackPos < buf.Length; i++, _playbackPos++)
                 {
-                    double sample = buf[_playbackPos];
-                    outL[i] += sample;
-                    outR[i] += sample;
+                    outL[i] += buf[_playbackPos];
+                    outR[i] += buf[_playbackPos];
                 }
 
                 if (_playbackPos >= buf.Length)
                 {
-                    Volatile.Write(ref _playbackBuffer, null);
+                    _playbackBuffer = null;
                     _playbackPos = 0;
                 }
             }
         }
-        catch (Exception ex)
+        catch
         {
-            System.Diagnostics.Debug.WriteLine($"Playback processing error: {ex.Message}");
         }
     }
 
